1e91eafbbacb0e9cb8d4b39d880c62703251d92c
DeenoDev <deyanoklaasen@outlook.com>
2025-07-03 23:58:04 +0200

    Heading styled with text size and color.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import React from "react";
 const App = () => {
   return (
     <main>
-      <h1>Welcome to Awwwards</h1>
+      <h1 className="text-5xl text-orange-500 font-bold">
+        Welcome to Awwwards
+      </h1>
     </main>
   );
 };
