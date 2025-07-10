2a9805debd3a0c1ecd132ba2edcd7a4f893d877b
DeenoDev <deyanoklaasen@outlook.com>
2025-07-10 23:52:57 +0200

    Rendered Hero.jsx component into App.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ import Hero from "./components/Hero";
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-violet-300 font-bold font-circular-web">
-        Welcome to Awwwards
-      </h1>
+      <Hero />
     </main>
   );
 };
